c463f5a7987a95c51ea1ed3897278edc0d8fd24c
Mr.KVM <kvmjava@yandex.ru>
2017-06-29 01:55:54 +0300

    начало 10 уровня

---
+++ task/task10/task1001/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task10.task1001;
+
+/* 
+Задача №1 на преобразование целых типов
+Расставьте правильно операторы приведения типа, чтобы получился ответ: d > 0
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = (byte) a + 46;
+        byte c = (byte) (a * b);
+        double f = (char) 1234.15;
+        long d = (short) (a + f / c + b);
+        System.out.println(d);
+    }
+}
