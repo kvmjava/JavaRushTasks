8d543614ef4ab332a882d6e629c5771d2bbf3fc1
Mr.KVM <kvmjava@yandex.ru>
2017-07-21 02:21:03 +0300

    Решение

---
+++ task/task10/task1011/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+        int a = 0;
+        while (a != 40) {
+            System.out.println(s.substring(a));
+            a++;
+        }
+        //напишите тут ваш код
+    }
+
+}
+
---
+++ task/task10/task1012/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task10.task1012;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/* 
+Количество букв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // алфавит
+        String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] abcArray = abc.toCharArray();
+
+        ArrayList<Character> alphabet = new ArrayList<Character>();
+        for (int i = 0; i < abcArray.length; i++) {
+            alphabet.add(abcArray[i]);
+        }
+
+        // ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+        // напишите тут ваш код
+
+        //вызываем метод подсчёта 1 символа в строке
+        for (Character ch : alphabet) {
+            System.out.println(ch + " " + schetCh());
+        }
+
+
+
+    }
+    //метод который подсчитывает кол-во встречающихся символов в 1 строке
+    private static int schetCh(){
+        int kolvo = 0;
+
+        return kolvo;
+    }
+}
