a50987b2c73e0a0c5bb5f64ceded6e0ed24a5b7e
Mr.KVM <kvmjava@yandex.ru>
2017-07-22 01:47:01 +0300

    Решение

--- task/task10/task1012/Solution.java
+++ task/task10/task1012/Solution.java
@@ -3,9 +3,9 @@ package com.javarush.task.task10.task1012;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Calendar;
 
-/* 
+
+/*
 Количество букв
 */
 
@@ -24,7 +24,7 @@ public class Solution {
 
         // ввод строк
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 2; i++) { //было 10
             String s = reader.readLine();
             list.add(s.toLowerCase());
         }
@@ -32,16 +32,17 @@ public class Solution {
 
         //вызываем метод подсчёта 1 символа в строке
         for (Character ch : alphabet) {
-            System.out.println(ch + " " + schetCh());
+            System.out.println(ch + " " + schetCh(list, ch));
         }
-
-
-
     }
     //метод который подсчитывает кол-во встречающихся символов в 1 строке
-    private static int schetCh(){
+    private static int schetCh(ArrayList<String> list, Character ch){
         int kolvo = 0;
-
+        for (String elementList : list) {
+            for (int i = 0; i < elementList.length(); i++) {
+                if (elementList.charAt(i) == ch) kolvo++;
+            }
+        }
         return kolvo;
     }
 }
